782713c36ca965435160c217d98afd7cbce81743
Ray Wagner <raywagner88@me.com>
2020-04-27 16:11:43 -0500

    update param

--- app/jobs/vue_job.rb
+++ app/jobs/vue_job.rb
@@ -8,7 +8,7 @@ class VueJob < ApplicationJob
   end
 
   def perform
-    resp = Shell.execute("cd ~/SycamoreSchoolVue; git checkout #{branch}; git pull; yarn lint --no-fix")
+    resp = Shell.execute("cd ~/SycamoreSchoolVue; git checkout #{@branch}; git pull; yarn lint --no-fix")
     client.create_status(@repo, @sha, resp.success? ? 'success' : 'failure', { context: 'Eslint Checks', description: resp.stdout })
   end
 
