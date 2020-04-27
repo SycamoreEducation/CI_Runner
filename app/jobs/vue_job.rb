bf25cf5e53be1d03f12a72ccc3a6c71696307b3b
Ray Wagner <raywagner88@me.com>
2020-04-27 16:33:37 -0500

    add yarn install

--- app/jobs/vue_job.rb
+++ app/jobs/vue_job.rb
@@ -10,6 +10,8 @@ class VueJob < ApplicationJob
   def perform
     resp = Shell.execute("cd ~/SycamoreSchoolVue; git checkout #{@branch}; git pull; yarn install; yarn lint --no-fix")
     Rails.logger.debug resp.success?
+    Rails.logger.debug resp.stdout
+    Rails.logger.debug resp.stderr
     client.create_status(@repo, @sha, resp.success? ? 'success' : 'failure', { context: 'Eslint Checks' })
   end
 
